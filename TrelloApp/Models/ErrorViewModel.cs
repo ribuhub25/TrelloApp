20f06e8c4db91acb43604339dadb4d2711b222b0
ribuhub25 <jhonatanribu25@gmail.com>
2024-08-14 23:08:50 -0500

    agregando comentario

--- TrelloApp/Models/ErrorViewModel.cs
+++ TrelloApp/Models/ErrorViewModel.cs
@@ -2,6 +2,7 @@ namespace TrelloApp.Models
 {
     public class ErrorViewModel
     {
+        //soy giliposha
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
