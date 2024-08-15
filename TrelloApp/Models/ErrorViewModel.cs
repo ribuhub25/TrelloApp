b63a21dceb3c7a0004b2844ab0bd67da35f827de
ribuhub25 <jhonatanribu25@gmail.com>
2024-08-14 23:10:43 -0500

    clogro

--- TrelloApp/Models/ErrorViewModel.cs
+++ TrelloApp/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace TrelloApp.Models
 {
     public class ErrorViewModel
     {
-        //soy giliposha
+  
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
